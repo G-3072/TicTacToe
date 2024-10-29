deddf3989a5d972a29d794a6cb21267dadfecf16
Gaudenz Schweizer <gaudenz.schweizer@cedes.com>
2024-10-29 13:47:14 +0100

    added textures

--- src/game/display.py
+++ src/game/display.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import os
 
 class display:
     def __init__(self):
@@ -8,7 +9,46 @@ class display:
 
         self.screen = pygame.display.set_mode((self.ScreenWidth, self.ScreenHeigth))
         
-        self.board = pygame.image.load("")
+        #declaring textures
+        self.board: pygame.Surface
+        self.O: pygame.Surface
+        self.X: pygame.Surface
+        
+        self.ButtonMultiplayer: pygame.Surface
+        self.ButtonSingleplayer: pygame.Surface
+        self.ButtonQuit: pygame.Surface
+        
+        self.ButtonRematchPlayer1: pygame.Surface
+        self.ButtonRematchPlayer2: pygame.Surface
+        self.ButtonRematchDraw: pygame.Surface
+        
+    
+    def _LoadTextures(self):
+        """
+        loading textures from assets directory
+        """
+        asset_dir = os.path.join(os.path.dirname(__file__), "assets")
+        
+        self.board = pygame.image.load(os.path.join(asset_dir, "board.png"))
+        self.O = pygame.image.load(os.path.join(asset_dir, "O.png"))
+        self.X = pygame.image.load(os.path.join(asset_dir, "X.png"))
+        
+        self.ButtonMultiplayer = pygame.image.load(os.path.join(asset_dir, "ButtonMultiplayer.png"))
+        self.ButtonSingleplayer = pygame.image.load(os.path.join(asset_dir, "ButtonSingleplayer.png"))
+        self.ButtonQuit = pygame.image.load(os.path.join(asset_dir, "ButtonQuit.png"))
+        
+        self.ButtonRematchPlayer1 = pygame.image.load(os.path.join(asset_dir, "ButtonRematchPlayer1.png"))
+        self.ButtonRematchPlayer2 = pygame.image.load(os.path.join(asset_dir, "ButtonRematchPlayer2.png"))
+        self.ButtonRematchDraw = pygame.image.load(os.path.join(asset_dir, "ButtonRematchDraw.png"))
+        
+
+        
+        
+        
+        
+        
+        
+        
 
     def DrawBoard(self):
         pass
