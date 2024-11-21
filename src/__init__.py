80edb8ea0b10bb322f90316ce5d813c9c4a826d4
Gaudenz Schweizer <gaudenz.schweizer@cedes.com>
2024-11-21 16:29:46 +0100

    updated __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,10 @@
 
-__all__ = ["display","game","playerInput","bot", "config"]
+__all__ = ["Game", "Display", "Board", "Human", "Bot", "Player", "config"]
 
 from .game import Game
-
+from .display import Display
+from .board import Board
+from .human import Human
+from .bot import Bot
+from ._player import Player
+from .config import *
